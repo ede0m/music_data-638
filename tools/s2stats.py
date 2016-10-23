8ee067bff54d94fd8983e5da4816006074821959
= <--replace-all>
2016-10-23 18:56:35 -0500

    Adding tempo and durations histograms

--- tools/s2stats.py
+++ tools/s2stats.py
@@ -21,6 +21,8 @@ mn_len_title = 1000
 
 len_artist_array = []
 len_title_array = []
+tempos = []
+durations = []
 
 av_tmpo = 0 
 av_dur = 0
@@ -60,6 +62,8 @@ with open(fp,'r') as data_file:
 		artist_len = len(artist)
 
 		len_artist_array.append(artist_len)
+		tempos.append(tempo)
+		durations.append(duration)
 
 
 		av_len_artist = av_len_artist + artist_len
@@ -112,49 +116,90 @@ quartile3Artist = len_artist_array[(len(len_artist_array) // 4) * 3]
 medianTitle = len_title_array[len(len_title_array) // 2]
 medianArtist = len_artist_array[len(len_artist_array) // 2]
 
+tempos.sort()
+durations.sort()
 
-print('\n\nAVG ARTIST LEN: ', av_len_artist)
-print('MX ARTIST LEN:  ', mx_len_artist)
-print('MN ARTIST LEN:  ', mn_len_artist)
-print('\n\nAVG TITLE LEN:  ' , av_len_title)
-print('MX TITLE LEN:   ', mx_len_title)
-print('MN TITLE LEN:   ', mn_len_title)
+q1Tempo = tempos[len(tempos) // 4]
+q3Tempo = tempos[(len(tempos)//4) * 3]
+
+q1Duration = durations[len(durations) // 4]
+q3Duration = durations[(len(durations) // 4) * 3]
+
+medianTempo = tempos[len(tempos) // 2]
+medianDuration = durations[len(durations) // 2]
+
 
 print('\n', count_dict )
 print('\nmissing_genres: ',genre_miss)
-print('\nAVG TEMPO:    ', av_tmpo)
-print('AVG DURATION: ', av_dur)
-print('MX TEMPO:     ', mx_tmpo)
-print('MX DURATION:  ', mx_dur, '\n\n')
 
-print("\nQ1 Title Length: ", quartile1Title)
+print('\nAVG ARTIST LEN: ', av_len_artist)
+print('MED ARTIST LEN:     ', medianArtist)
+print('MX ARTIST LEN:  ', mx_len_artist)
+print('MN ARTIST LEN:  ', mn_len_artist)
+print('Q1 ARTIST LEN:  ', quartile1Artist)
+print('Q3 ARTIST LEN:  ', quartile3Artist)
+
+print('\nAVG TITLE LEN:  ' , av_len_title)
+print('MED TITLE LEN:      ', medianTitle)
+print('MX TITLE LEN:   ', mx_len_title)
+print('MN TITLE LEN:   ', mn_len_title)
+print("Q1 Title Length: ", quartile1Title)
 print("Q3 Title Length: ", quartile3Title)
 
-print("\nQ1 Artist Length: ", quartile1Artist)
-print("Q3 Artist Length: ", quartile3Artist)
+print('\nAVG TEMPO:    ', av_tmpo)
+print('MED TEMPO:      ', medianTempo)
+print('MX TEMPO:     ', mx_tmpo)
+print('MN TEMPO:     ', tempos[0])
+print('Q1 TEMPO:     ', q1Tempo)
+print('Q3 TEMPO:     ', q3Tempo)
+
+print('\nAVG DURATION: ', av_dur)
+print('MED DURATION:   ', medianDuration)
+print('MX DURATION:  ', mx_dur)
+print('MN DURATION:  ', durations[0])
+print('Q1 DURATION:  ', q1Duration)
+print('Q3 DURATION:  ', q3Duration)
 
 
 #Uncomment each one you want to show. If you uncomment multiple the code hangs until
 #you close the tkinter window.
 
-# BINS_Title = list(range(0,40))
+#Song title length hist
+# BINS_Title = list(range(0,int(mx_len_title)))
 # plt.hist(len_title_array,bins = BINS_Title)
 # plt.xlabel("Song Title Length in Characters")
 # plt.ylabel("Instances")
 # plt.title("Histogram Song Title Length")
 # plt.show()
 
-
-# BINS_Artist = list(range(0,55))
+#Artist Len histogram
+# BINS_Artist = list(range(0,int(mx_len_artist)))
 # plt.hist(len_artist_array, bins = BINS_Artist)
 # plt.xlabel("Artist Length in Characters")
 # plt.ylabel("Instances")
 # plt.title("Histogram Artist Length")
 # plt.show()
 
-plt.bar(range(len(count_dict)), count_dict.values(), align='center')
-plt.xticks(range(len(count_dict)), count_dict.keys())
-plt.title("Bar Chart of Genres")
-plt.xlabel("Genre")
-plt.ylabel("Instance of Genre")
+#Tempo Histogram
+TEMPO_BINS = list(range(0,int(mx_tmpo)))
+plt.hist(tempos, bins=TEMPO_BINS)
+plt.title('Histogram Tempo per Song')
+plt.xlabel('Tempo (Beats Per Minute)')
+plt.ylabel('Instances')
 plt.show()
+
+#Duration Histogram
+DURATION_BINS = list(range(0,int(mx_dur)))
+plt.hist(durations, bins= DURATION_BINS)
+plt.title("Durations Of Songs")
+plt.xlabel("Duration (in seconds)")
+plt.ylabel("Instances")
+plt.show()
+#Genre bar chart
+# plt.bar(range(len(count_dict)), count_dict.values(), align='center')
+# plt.xticks(range(len(count_dict)), count_dict.keys())
+# plt.title("Bar Chart of Genres")
+# plt.xlabel("Genre")
+# plt.ylabel("Instance of Genre")
+# plt.show()
+
