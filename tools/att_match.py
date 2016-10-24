187147db72b2f219af8641b755e0d73aad41987a
Garritt Moede <garritt@Garritts-MacBook-Air.local>
2016-10-23 23:24:22 -0500

    more tables

--- tools/att_match.py
+++ tools/att_match.py
@@ -13,8 +13,8 @@
 import os
 
 
-data_file_path_1 = "../csv/dump_CSV_sample.csv"
-#data_file_path_1 = "../csv/MSD/msd_2.csv"
+#data_file_path_1 = "../csv/dump_CSV_sample.csv"
+data_file_path_1 = "../csv/MSD/msd_6.csv"
 fp = os.path.relpath(data_file_path_1, os.curdir)
 
 data_file_path_2 = "../csv/usable_data.csv"
@@ -22,7 +22,7 @@ fp2 = os.path.relpath(data_file_path_2, os.curdir)
 
 matches = 0
 usable_data = []
-table_A = open("../csv/tables/table_sample.csv", 'w')
+table_A = open("../csv/tables/table_6.csv", 'w')
 table_A.write("Artist, Song, Genre, Tempo, Duration\n")
 
 with open(fp2,'r') as data_file:
