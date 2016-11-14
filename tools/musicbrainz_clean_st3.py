4dcebe6562152b6eaba9101f10f88a3c2ccfc45b
Garritt Moede <garritt@Garritts-MacBook-Air.local>
2016-11-13 20:20:54 -0600

    reorganize

--- tools/musicbrainz_clean_st3.py
+++ tools/musicbrainz_clean_st3.py
@@ -1,11 +1,16 @@
 import os, sys, csv
 
-print("here")
-with open("musicbrainz_final.csv") as csvfile, open("csv_out.csv", "w") as out:
+count = 0
+
+with open("../csv/musicbrainz_old.csv") as csvfile, open("musicbrainz_clean_st3.csv", "w") as out:
     csvreader = csv.reader(csvfile)
     csvwriter = csv.writer(out)
     for row in csvreader:
         ## split = row.split(",")
         if len(row) == 4:
             csvwriter.writerow(row)
+        else:
+        	count+=1
+
+print('Removed ', count, ' tuples\n')
 
