8226586d5c030cdec6cdefdfc498e288b774a199
Evan K <ekivolowitz@wisc.edu>
2016-11-28 22:49:46 -0600

    Commenting code.

--- csv/decision_tree.py
+++ csv/decision_tree.py
@@ -1,15 +1,35 @@
 from pandas import DataFrame
 import os
 import subprocess
-from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.tree import DecisionTreeClassifier
+
+
+
+#Dev set to set up tree.
 df = DataFrame.from_csv("training/dev_set.csv")
+
+
+#Sets the columns of the vectors we use for ML. Does not include ID column.
+#id,atable_id,btable_id,sim_title,sim_artist,dif_year,match
+#ID is ignored because DataFram.from_csv defaults to having the first column as the ID column. 
+#atable_id, btable_id ignored in the following line. Match ignored in the following line because the 2:5 is inclusive:exclusive
 features = list(df.columns[2:5])
+
+
+#I don't know what the min_samples_split, random_state are, I just googled it and picked arbitrary numbers.
 dt = DecisionTreeClassifier(min_samples_split=100, random_state=99)
+
+#Per vector, learn from the features | the match value of that vector.
 tree = dt.fit(df[features], df['match'])
 
+
+#Model was learned above, now apply to unknown data.
 dTestFrame = DataFrame.from_csv("training/eval_set.csv")
 output = (tree.predict(dTestFrame[features]))
 
+
+
+#Evaluating the results. 
 eval_results = []
 with open("training/eval_set.csv", 'r') as f:
 	for line in f:
